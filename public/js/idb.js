62fe6ada510d22290c517c3094c81c88284ed191
Derek Beckford <dbeck1207@icloud.com>
2021-09-19 18:42:18 -0500

    refractor

--- public/js/idb.js
+++ public/js/idb.js
@@ -53,7 +53,7 @@ function uploadTransactions() {
           alert("All transactions have been submitted!");
         })
         .catch((err) => {
-          console.log(err.Error.message);
+          console.log(json.stringify(err));
         });
     }
   };
